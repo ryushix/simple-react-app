988b9bbbfbc0adb0780fca2585ecb5efe0613007
Rafi Al Ayyubi <122455865+ryushix@users.noreply.github.com>
2024-10-20 12:56:52 +0800

    little change

--- app/functions/index.js
+++ app/functions/index.js
@@ -2,17 +2,37 @@ const { Storage } = require('@google-cloud/storage');
 const storage = new Storage();
 
 exports.uploadFile = async(req, res) => {
-    try {
-        const bucketName = 'NAMA_BUCKET_ANDA'; // Ganti dengan nama bucket Anda
-        const file = req.body.file; // File harus diupload sebagai base64 string
-        const buffer = Buffer.from(file, 'base64');
-        const fileName = req.body.fileName; // Nama file yang akan disimpan
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Methods', 'POST');
 
-        await storage.bucket(bucketName).file(fileName).save(buffer, {
-            contentType: 'application/octet-stream', // Ganti dengan contentType yang sesuai
+    if (req.method === 'OPTIONS') {
+        res.set('Access-Control-Allow-Headers', 'Content-Type');
+        return res.status(200).send();
+    }
+
+    try {
+        const bucketName = 'NAMA_BUCKET_ANDA'; // change bucket name
+        const file = req.body.file;
+        const buffer = Buffer.from(file, 'base64');
+        const fileName = req.body.fileName; // file name that in saved
+
+        const fileUpload = storage.bucket(bucketName).file(fileName);
+
+        // save to the bucket
+        await fileUpload.save(buffer, {
+            contentType: 'application/octet-stream',
         });
 
-        res.status(200).send('File uploaded successfully.');
+        // make a public
+        await fileUpload.makePublic();
+
+        // get UrlSigned
+        const publicUrl = `https://storage.googleapis.com/${bucketName}/${fileName}`;
+
+        res.status(200).send({
+            message: 'File uploaded successfully.',
+            publicUrl: publicUrl,
+        });
     } catch (error) {
         console.error(error);
         res.status(500).send('Error uploading file.');
--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import FileUpload from './FileUpload'; // Impor komponen FileUpload
+import FileUpload from './FileUpload'; 
 
 function App() {
     return (
         <div className="App">
             <h1>File Upload to Google Cloud Storage</h1>
-            <FileUpload /> {/* Tambahkan komponen FileUpload */}
+            <FileUpload /> {}
         </div>
     );
 }
